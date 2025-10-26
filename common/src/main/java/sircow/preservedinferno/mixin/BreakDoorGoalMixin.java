d58d2d3230a95a82f4bdd424c2da05a7d7207687
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2025-10-26 22:04:21 +0000

    beta-1.2.2.1
    + hotfix to make fence gates actually break

--- common/src/main/java/sircow/preservedinferno/mixin/BreakDoorGoalMixin.java
+++ common/src/main/java/sircow/preservedinferno/mixin/BreakDoorGoalMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,6 +21,12 @@ import sircow.preservedinferno.other.ModTags;
 
 @Mixin(BreakDoorGoal.class)
 public abstract class BreakDoorGoalMixin extends DoorInteractGoal {
+    @Shadow protected int breakTime;
+    @Shadow protected int lastBreakProgress;
+
+    @Shadow protected abstract int getDoorBreakTime();
+    @Shadow public abstract void stop();
+
     public BreakDoorGoalMixin(Mob mob) {
         super(mob);
     }
@@ -27,7 +34,7 @@ public abstract class BreakDoorGoalMixin extends DoorInteractGoal {
     @ModifyReturnValue(method = "getDoorBreakTime", at = @At("RETURN"))
     private int preserved_inferno$doorBreakTime(int original) {
         BlockState state = this.mob.level().getBlockState(this.doorPos);
-        if (state.is(ModTags.REINFORCED_DOORS_BLOCK)) return Integer.MAX_VALUE; // can't break
+        if (state.is(ModTags.REINFORCED_DOORS_BLOCK)) return Integer.MAX_VALUE;
         if (state.is(ModTags.COPPER_DOORS)) return 15 * 20;
         if (state.is(BlockTags.WOODEN_DOORS)) return 6 * 20;
         return original;
@@ -35,88 +42,59 @@ public abstract class BreakDoorGoalMixin extends DoorInteractGoal {
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void preserved_inferno$checkDoorTypeDist(CallbackInfo ci) {
-        BlockState state = this.mob.level().getBlockState(this.doorPos);
-        if (state.is(ModTags.REINFORCED_DOORS_BLOCK)) ci.cancel();
+        var level = this.mob.level();
+        var pos = this.doorPos;
+        var state = level.getBlockState(pos);
+        var block = state.getBlock();
 
-        if (state.getBlock() instanceof DoorBlock door) {
+        if (block instanceof DoorBlock door) {
             if (state.getValue(DoorBlock.HALF) == DoubleBlockHalf.UPPER) {
                 var lower = doorPos.below();
                 if (this.mob.level().getBlockState(lower).getBlock() == door)
                     this.doorPos = lower;
             }
         }
-
         double distanceSq = this.mob.distanceToSqr(this.doorPos.getX() + 0.5D, this.doorPos.getY(), this.doorPos.getZ() + 0.5D);
         double rangeSq = 1.5D * 1.5D;
 
         if (distanceSq > rangeSq) {
             this.mob.getNavigation().moveTo(this.doorPos.getX() + 0.5D, this.doorPos.getY(), this.doorPos.getZ() + 0.5D, 1.0D);
         }
-    }
 
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void preserved_inferno$break(CallbackInfo ci) {
-        BreakDoorGoal self = (BreakDoorGoal) (Object) this;
-        var level = this.mob.level();
-        var pos = this.doorPos;
-        if (pos == null || level == null) return;
+        if (state.is(ModTags.REINFORCED_DOORS_BLOCK)) {
+            ci.cancel();
+            return;
+        }
 
-        var state = level.getBlockState(pos);
-        var block = state.getBlock();
-        if (!(block instanceof DoorBlock) && !(block instanceof FenceGateBlock)) return;
+        if (block instanceof FenceGateBlock) {
+            this.breakTime++;
 
-        try {
-            var breakTimeField = BreakDoorGoal.class.getDeclaredField("breakTime");
-            var doorBreakTimeField = BreakDoorGoal.class.getDeclaredField("doorBreakTime");
-            breakTimeField.setAccessible(true);
-            doorBreakTimeField.setAccessible(true);
-
-            int breakTime = breakTimeField.getInt(self);
-            int required = Math.max(5 * 20, (int) doorBreakTimeField.get(self));
-
-            if (breakTime >= required) {
-                boolean broken = false;
-
-                if (block instanceof DoorBlock door) {
-                    DoubleBlockHalf half = state.getValue(DoorBlock.HALF);
-                    BlockPos lower = half == DoubleBlockHalf.UPPER ? pos.below() : pos;
-                    BlockPos upper = half == DoubleBlockHalf.UPPER ? pos : pos.above();
-                    BlockState lowerState = level.getBlockState(lower);
-                    BlockState upperState = level.getBlockState(upper);
-
-                    if (lowerState.getBlock() == door) {
-                        level.levelEvent(1021, lower, 0);
-                        level.levelEvent(2001, lower, Block.getId(lowerState));
-                        level.removeBlock(lower, false);
-                        broken = true;
-                    }
-                    if (upperState.getBlock() == door) {
-                        level.levelEvent(2001, upper, Block.getId(upperState));
-                        level.removeBlock(upper, false);
-                        broken = true;
-                    }
-                } else if (block instanceof FenceGateBlock) {
-                    level.levelEvent(1021, pos, 0);
-                    level.levelEvent(2001, pos, Block.getId(state));
-                    level.removeBlock(pos, false);
-                    broken = true;
-                }
-
-                if (broken) {
-                    this.hasDoor = false;
-                    this.doorPos = this.mob.blockPosition();
-                    try {
-                        var breakingTimeField = BreakDoorGoal.class.getDeclaredField("breakTime");
-                        breakingTimeField.setAccessible(true);
-                        breakingTimeField.setInt(self, 0);
-                    }
-                    catch (ReflectiveOperationException ignored) {}
-
-                    self.stop();
+            if (this.mob.getRandom().nextInt(20) == 0) {
+                this.mob.level().levelEvent(1019, this.doorPos, 0);
+                if (!this.mob.swinging) {
+                    this.mob.swing(this.mob.getUsedItemHand());
                 }
             }
+
+            int required = this.getDoorBreakTime();
+            int i = (int)((float)this.breakTime / required * 10.0F);
+            if (i != this.lastBreakProgress) {
+                this.mob.level().destroyBlockProgress(this.mob.getId(), this.doorPos, i);
+                this.lastBreakProgress = i;
+            }
+
+            if (this.breakTime >= required) {
+                level.levelEvent(1021, pos, 0);
+                level.levelEvent(2001, pos, Block.getId(state));
+                level.removeBlock(pos, false);
+
+                this.hasDoor = false;
+                this.doorPos = this.mob.blockPosition();
+                this.breakTime = 0;
+                this.stop();
+            }
+            ci.cancel();
         }
-        catch (ReflectiveOperationException ignored) {}
     }
 
     @Inject(method = "canUse", at = @At("RETURN"), cancellable = true)
@@ -155,17 +133,23 @@ public abstract class BreakDoorGoalMixin extends DoorInteractGoal {
     }
 
     @Inject(method = "canContinueToUse", at = @At("HEAD"), cancellable = true)
-    private void preserved_inferno$stopIfDoorGone(CallbackInfoReturnable<Boolean> cir) {
+    private void preserved_inferno$stopIfFenceGateGone(CallbackInfoReturnable<Boolean> cir) {
         if (!this.hasDoor) {
             cir.setReturnValue(false);
             return;
         }
 
         var state = this.mob.level().getBlockState(this.doorPos);
-        if (!(state.getBlock() instanceof DoorBlock || state.getBlock() instanceof FenceGateBlock)) {
-            this.hasDoor = false;
-            this.doorPos = this.mob.blockPosition();
-            cir.setReturnValue(false);
+
+        if (state.getBlock() instanceof FenceGateBlock) {
+            if (!(state.getBlock() instanceof FenceGateBlock) || this.breakTime > this.getDoorBreakTime()) {
+                this.hasDoor = false;
+                this.doorPos = this.mob.blockPosition();
+                cir.setReturnValue(false);
+            }
+            else {
+                cir.setReturnValue(true);
+            }
         }
     }
 }
